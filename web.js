17c722a6769bb6d583864bd47583ade7ad461558
Sunil Dumpala <dumpala@gmail.com>
2013-07-09 06:17:25 +0000

    Update web.js to read from Index file

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs = require('fs');
+
+var data = new Buffer( fs.readFileSync('index.html'), "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
